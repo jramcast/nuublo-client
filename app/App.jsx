ba7fc063cb408ad49f91feed7cb39b76c37400c7
Jaime Ramírez <jaime.ramirez@roiback.com>
2017-08-29 08:44:35 +0200

    chore(app): Setup base react and wepback app

---
+++ app/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
+
+export default class App extends Component {
+
+    render() {
+        return (
+        <div>
+            <h1>Hello World!!! {this.props.name}</h1>
+        </div>
+        );
+    }
+
+} 
---
+++ app/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { SlideIn } from 'react-slidein';
+
+import App from './App.jsx';
+
+ReactDOM.render(
+    <App name="Ues" />,
+    document.getElementById('app')
+);
+
+let open = false;
+document.getElementById('slideinBtn').addEventListener('click', () => {
+    open = !open;
+    console.log(open);
+    ReactDOM.render(
+        <SlideIn className={'my-dropdown-slidein'}>
+            {open ? getCHildrenElement() : null}
+        </SlideIn>,
+        document.getElementById('slidein')
+    );
+
+});
+
+function getCHildrenElement() {
+    return (
+        <div style={{ background: 'red' }}>
+            <h2> Otro componente </h2>
+            <p> 'Webhooks can be used for binding events when something is happening within the project.' </p>
+        </div>
+    );
+}
